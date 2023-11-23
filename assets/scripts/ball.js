6125435e0abfc5c1b5fbd18b36d1d6e49f80d7d6
Crim6864 <>
2023-11-23 18:13:30 +0000

    added display blocks and drawing of the game

--- assets/scripts/ball.js
+++ assets/scripts/ball.js
@@ -1,8 +1,8 @@
 // Setting the 2d field and color.
 const WIDTH = 500,
         HEIGHT = 500,
-        canvas = document.getElementById('canvas'), 
-        context = canvas.getContext('2d');
+        canvas=document.getElementById('canvas'), 
+        context=canvas.getContext('2d');
 
 canvas.width=WIDTH;
 canvas.height=HEIGHT;
@@ -22,11 +22,14 @@ var blockProp={
     h:padding
 }
 
+InitializeBlocks();
+
 // Initialize the blocks
 function InitializeBlocks() {
-    for(let i=0;i<numBlockCol;i++){
+    for(let i=1;i<numBlockCol;i++){
         for(let j=0;j<numBlockRow;j++){
-            blockArr[blockIndex]=new Block(padding/2+(i+1)*(colWidth)-colWidth/2,colWidth+j*(colWidth-2*padding),blockProp.w-padding,blockProp.h,true)
+            blockArr[blockIndex]=new Block(padding/2+(i)*(colWidth)-colWidth/2,colWidth+j*(colWidth-2*padding),blockProp.w-padding,blockProp.h,true);
+            blockIndex++;
         }
     }
 }
@@ -39,3 +42,22 @@ function Block(x, y, width, height, isShow){
     this.height=height;
     this.isShow=isShow;
 }
+
+// Display the Blocks
+function DisplayBlocks(){
+    blockArr.forEach((b)=>{
+        if(b.isShow){
+            context.beginPath();
+            context.fillStyle='#E15517';
+            context.fillRect(b.x, b.y, b.width, b.height);
+            context.closePath();
+        }
+    });
+}
+
+// Draws the Game
+function drawGame(){
+    DisplayBlocks();
+}
+
+drawGame();
